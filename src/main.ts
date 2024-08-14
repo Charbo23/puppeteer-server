6ff4f832fd81b91107d31c1de7a6e04c37c83b9c
ChenYibo <charbo233@foxmail.com>
2024-08-14 17:01:25 +0800

    图片生成优化

--- src/main.ts
+++ src/main.ts
@@ -16,9 +16,9 @@ const initPuppeteerPool = () => {
         min: 1, //保证池中最少有多少个实例存活
         testOnBorrow: true, // 在将 实例 提供给用户之前，池应该验证这些实例。
         autostart: false, //是不是需要在 池 初始化时 初始化 实例
-        idleTimeoutMillis: 1000 * 60, //如果一个实例 60分钟 都没访问就关掉他
-        evictionRunIntervalMillis: 1000 * 3, //每 3分钟 检查一次 实例的访问状态
-        maxUses: 2048, //自定义的属性：每一个 实例 最大可重用次数。
+        idleTimeoutMillis: 1000*60 * 60, //如果一个实例 60分钟 都没访问就关掉他
+        evictionRunIntervalMillis: 1000 *60* 3, //每 3分钟 检查一次 实例的访问状态
+        maxUses: 1000, //自定义的属性：每一个 实例 最大可重用次数。
         validator: (instance) => {
             if (!instance.isConnected()) {
                 console.log("实例已不活跃");
--- src/services/generate-image.ts
+++ src/services/generate-image.ts
@@ -1,5 +1,5 @@
 import type { Context } from "koa";
-
+import { nanoid } from "nanoid";
 /**
  * @example
 
@@ -18,33 +18,57 @@ export default class GenerateImageService {
             };
         }
 
-        const browser =  await global.pp.use();
+        const browser = await global.pp.use();
 
         const page = await browser.newPage();
 
         await page.setUserAgent(
-            "Mozilla/5.0 (iPhone; CPU iPhone OS 17_2_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148"
+            "Mozilla/5.0 (iPhone; CPU iPhone OS 17_2_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Puppeteer"
         );
         await page.setViewport({
-            width: 393,
-            height: 852,
+            width: 375,
+            height: 812,
             deviceScaleFactor: 3,
             isMobile: true,
-            hasTouch: true,
+            // hasTouch: true,
         });
         const url = ctx.query.url;
+        const rquestId = nanoid(8);
+        console.time("页面加载时间" + rquestId);
         await page.goto(ctx.query.url as string, {
             waitUntil: "networkidle2",
+            // waitUntil:'domcontentloaded'
         });
-        if (url.includes("page=timu")) {
-            // 等待页面内时机
-            await page.waitForFunction(() => window.readyForScreenshot === true);
+        console.timeEnd("页面加载时间" + rquestId);
+        let pageLoadState = "success";
+        if (url.includes("page=timu") || url.includes("question_share")) {
+            // 特定页面需等待页面内时机
+            try {
+                await page.waitForFunction(
+                    () => window.pageLoadState !== undefined,
+                    { timeout: 7000 }
+                );
+                pageLoadState = await page.evaluate(() => window.pageLoadState);
+            } catch (e) {
+                pageLoadState = "error";
+            }
         }
-      const img= await page.screenshot({
-            fullPage: true,
-            type: "png",
-        });
-        await page.close()
-        return img
+        if (pageLoadState == "success") {
+            // 页面加载成功
+            const bodyHandle = await page.$("body"); //只截取body
+            const img = await bodyHandle.screenshot({
+                // fullPage: true,
+                type: "png",
+            });
+            await page.close();
+            return img;
+        } else {
+            // 页面加载失败
+            await page.close();
+            return {
+                code: 4002,
+                msg: "页面加载失败",
+            };
+        }
     };
 }
