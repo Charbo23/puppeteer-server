d6b65b40bb92257124b88fe594bd6899f71ed602
ChenYibo <charbo233@foxmail.com>
2024-08-14 18:05:30 +0800

    去除冗余代码

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,5 @@
 import homeController from '@/controllers/home'
 import GenerateImageController from '@/controllers/generate-image'
-import GeneratePdfController from '@/controllers/generate-pdf'
-import GenerateSimplePdfController from '@/controllers/generate-simple-pdf'
-import GenerateCombinePdfController from '@/controllers/generate-combine-pdf'
 import type Router from 'koa-router'
 
 type HttpMethodKeys = Extract<keyof Router,
@@ -30,20 +27,5 @@ const routes: Array<RouteConfig> = [
     method: 'get',
     action: GenerateImageController.generate
   },
-  {
-    path: '/pdf',
-    method: 'post',
-    action: GeneratePdfController.generate
-  },
-  {
-    path: '/simple-pdf',
-    method: 'get',
-    action: GenerateSimplePdfController.generate
-  },
-  {
-    path: '/combine-pdf',
-    method: 'post',
-    action: GenerateCombinePdfController.generate
-  }
 ]
 export default routes
