9aefaf069d9de4e1a3e146ab303bce71a7de3bc3
ChenYibo <charbo233@foxmail.com>
2024-08-15 14:14:43 +0800

    优化连接池

--- src/main.ts
+++ src/main.ts
@@ -10,14 +10,16 @@ import genericPool from "generic-pool";
 
 // puppeteer连接池
 const initPuppeteerPool = () => {
-    if (global.pp) global.pp.drain().then(() => global.pp.clear());
+    if (global.pp) {
+        global.pp.drain().then(() => global.pp.clear());
+    }
     const opt = {
         max: 4, //最多产生多少个 puppeteer 实例 。
         min: 1, //保证池中最少有多少个实例存活
         testOnBorrow: true, // 在将 实例 提供给用户之前，池应该验证这些实例。
         autostart: false, //是不是需要在 池 初始化时 初始化 实例
-        idleTimeoutMillis: 1000*60 * 60, //如果一个实例 60分钟 都没访问就关掉他
-        evictionRunIntervalMillis: 1000 *60* 3, //每 3分钟 检查一次 实例的访问状态
+        idleTimeoutMillis: 1000 * 60 * 10, //如果一个实例 10分钟 都没访问就关掉他
+        evictionRunIntervalMillis: 1000 * 60 * 1, //每 1分钟 检查一次 实例的访问状态
         maxUses: 1000, //自定义的属性：每一个 实例 最大可重用次数。
         validator: (instance) => {
             if (!instance.isConnected()) {
